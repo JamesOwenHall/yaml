36cba13a0520df6d546f941c650a7bfcbacda3a0
James Hall <james_hall@live.ca>
2026-04-05 14:18:53 -0400

    Add basic parsing test

--- src/test.zig
+++ src/test.zig
@@ -1,3 +1,4 @@
+//! This module runs basic sanity checks against the LibYAML C library to ensure it builds correctly.
 const std = @import("std");
 const libyaml = @import("libyaml");
 
@@ -16,4 +17,53 @@ test "get_version" {
     try std.testing.expectEqual(0, major);
     try std.testing.expectEqual(2, minor);
     try std.testing.expectEqual(5, patch);
+}
+
+test "parse string" {
+    const gpa = std.testing.allocator;
+    const input =
+        \\foo:
+        \\  bar: baz
+        \\  jib: jab
+    ;
+
+    var parser: libyaml.yaml_parser_t = undefined;
+    std.debug.assert(libyaml.yaml_parser_initialize(&parser) == 1);
+    defer libyaml.yaml_parser_delete(&parser);
+
+    libyaml.yaml_parser_set_input_string(&parser, input, input.len);
+
+    var event_types = std.ArrayList(libyaml.yaml_event_type_t).empty;
+    defer event_types.deinit(gpa);
+
+    while (true) {
+        var event: libyaml.yaml_event_t = undefined;
+        if (libyaml.yaml_parser_parse(&parser, &event) != 1) {
+            return error.ParseError;
+        }
+        defer libyaml.yaml_event_delete(&event);
+
+        try event_types.append(gpa, event.type);
+        if (event.type == libyaml.YAML_STREAM_END_EVENT) {
+            break;
+        }
+    }
+
+    const expected = [_]libyaml.yaml_event_type_t{
+        libyaml.YAML_STREAM_START_EVENT,
+        libyaml.YAML_DOCUMENT_START_EVENT,
+        libyaml.YAML_MAPPING_START_EVENT,
+        libyaml.YAML_SCALAR_EVENT,
+        libyaml.YAML_MAPPING_START_EVENT,
+        libyaml.YAML_SCALAR_EVENT,
+        libyaml.YAML_SCALAR_EVENT,
+        libyaml.YAML_SCALAR_EVENT,
+        libyaml.YAML_SCALAR_EVENT,
+        libyaml.YAML_MAPPING_END_EVENT,
+        libyaml.YAML_MAPPING_END_EVENT,
+        libyaml.YAML_DOCUMENT_END_EVENT,
+        libyaml.YAML_STREAM_END_EVENT,
+    };
+
+    try std.testing.expectEqualSlices(libyaml.yaml_event_type_t, &expected, event_types.items);
 }
